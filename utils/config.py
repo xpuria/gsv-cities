e90a6cdbaac0a477b00cde6b0ed1e65be47b2284
pouria mohammadalipourahari <aharipuria1@gmail.com>
2025-01-18 02:07:39 +0100

    redirection of paths removed

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,3 @@
-!ln -s "/content/drive/MyDrive/gsv_cities_data" "/content/gsv-cities/datasets"
-
-
 GT_ROOT = "/content/drive/MyDrive/gsv_cities_data/"
 GSV_XS_PATH = "/content/drive/MyDrive/gsv_cities_data/gsv_xs/"
 SF_XS_PATH = "/content/drive/MyDrive/gsv_cities_data/sf_xs/"

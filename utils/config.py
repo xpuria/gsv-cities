27a3cbac07031a6f7ae98d3d609c0ea9896423a6
pouria mohammadalipourahari <aharipuria1@gmail.com>
2025-01-19 02:23:29 +0100

    config paths changed

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-GT_ROOT = "/content/datasets/"
-GSV_XS_PATH = "/content/datasets/gsv_xs/"
-SF_XS_PATH = "/content/datasets/sf_xs/"
-TOKYO_XS_PATH = "/content/datasets/tokyo_xs/"
+GT_ROOT = "/content/gsv-cities-dataset/datasets"
+GSV_XS_PATH = "/content/gsv-cities-dataset/datasets/gsv_xs"
+SF_XS_PATH = "/content/gsv-cities-dataset/datasets/sf_xs"
+TOKYO_XS_PATH = "/content/gsv-cities-dataset/datasets/tokyo_xs"

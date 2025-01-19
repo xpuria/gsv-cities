52086106fa2abd8f6f761aedb9e37868ed76c15e
xpuria <aharipuria1@gmail.com>
2025-01-19 03:03:28 +0100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-GT_ROOT = "/content/gsv-cities-dataset/datasets"
-GSV_XS_PATH = "/content/gsv-cities-dataset/datasets/gsv_xs"
-SF_XS_PATH = "/content/gsv-cities-dataset/datasets/sf_xs"
-TOKYO_XS_PATH = "/content/gsv-cities-dataset/datasets/tokyo_xs"
+GT_ROOT = "/content/gsv-cities/datasets/"
+GSV_XS_PATH = "/content/gsv-cities/gsv_xs"
+SF_XS_PATH = "/content/gsv-cities/sf_xs"
+TOKYO_XS_PATH = "/content/gsv-cities/tokyo_xs"
